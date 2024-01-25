c35205972bdc1dec227cb3e4d12f4fa7caac4abb
Matvei Roshchin <matvei_roshchin@epam.com>
2024-01-25 18:46:18 -0500

    Week1 first commit

---
+++ terrademo/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+    credentials = "./keys/my-creds.json"
+  project     = "aqueous-radio-411522"
+  region      = "us-central1"
+  }
+
+
+  resource "google_storage_bucket" "demo-bucket" {
+  name          = "aqueous-radio-411522-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
